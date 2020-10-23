5cb82d040d9532753961eaaadebfc207e107b3df
Pedro Ottoni Carneiro <pedrottoni@gmail.com>
2020-10-23 00:54:07 -0300

    Initial commit

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,12 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 14);
+
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
+).addTo(mymap);
+
+L.marker([51.5, -0.09]).addTo(mymap)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
